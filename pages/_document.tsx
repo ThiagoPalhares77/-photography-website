2b8bf9647c522500ddb8e91a1fc6a506ccbd4a59
Thiago <ps-thiago@hotmail.com>
2022-10-07 11:50:16 -0300

    Feat: Hero component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
