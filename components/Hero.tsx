b565f464209f141d0757602df908c3e91a2030b3
Thiago <ps-thiago@hotmail.com>
2022-10-08 14:08:55 -0300

    Feat: Scroll NavBar colors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Hero = ({ heading, message }) => {
   return (
-    <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover photo-img">
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover photo-img">
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
-      <div className="p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]">
+      <div className="p-5 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
         <button className="px-8 py-2 border">Livro</button>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,41 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 export const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const [color, setColor] = useState('transparent')
+  const [textColor, setTextColor] = useState('white')
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor('#FFF')
+        setTextColor('#000')
+      } else {
+        setColor('transparent')
+        setTextColor('#fff')
+      }
+    }
+    window.addEventListener('scroll', changeColor)
+  }, [])
+
   return (
-    <div className="fixed left-0 right-0 w-full z-10 ease-in duration-300">
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 right-0 w-full z-10 ease-in duration-300"
+    >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href={'/'}>
-          <h1 className="font-bold text-4xl ">Logo</h1>
+          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl ">
+            Logo
+          </h1>
         </Link>
-        <ul className="hidden sm:flex">
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
           <li className="p-4 ">
             <Link href={'/'}>Home</Link>
           </li>
@@ -25,13 +51,23 @@ export const Navbar = () => {
         </ul>
 
         {/* MOBILE  */}
-        <div className="block sm:hidden z-10 ">
-          <AiOutlineMenu scale={20} />
+        <div onClick={handleNav} className="block sm:hidden z-10 ">
+          {nav ? (
+            <AiOutlineClose scale={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu scale={20} style={{ color: `${textColor}` }} />
+          )}
         </div>
 
         {/* MOBILE MENU */}
 
-        <div className="sm:hidden absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300">
+        <div
+          className={
+            nav
+              ? 'sm:hidden absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              : 'sm:hidden absolute top-0 bottom-0 right-0 left-[-100%] flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          }
+        >
           <ul>
             <li className="p-4 text-4xl hover:text-gray-500">
               <Link href={'/'}>Home</Link>
