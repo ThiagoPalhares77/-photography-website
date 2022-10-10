f6c03d14c0fe4099864fb58b949008b268c4eb6a
Thiago <ps-thiago@hotmail.com>
2022-10-10 17:19:59 -0300

    Feat: Work page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import { Hero } from '../components/Hero'
 import { Instagram } from '../components/Instagram'
 import { Slider } from '../components/Slider'
---
+++ pages/work.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Hero } from '../components/Hero'
+
+export default function work() {
+  return (
+    <div>
+      <Hero
+        heading={'Meus trabalhos'}
+        message={
+          'Aqui estão alguns de meus recentes trabalhos ao redor do mundo'
+        }
+      />
+    </div>
+  )
+}
